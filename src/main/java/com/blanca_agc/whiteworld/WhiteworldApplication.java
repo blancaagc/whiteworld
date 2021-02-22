1316869d3f8a0ce9c2d5cd35c086534a206987e7
Blanca Galindo <blanca.galindo@agilethought.com>
2021-02-22 12:44:19 -0600

    Basic Container

---
+++ src/main/java/com/blanca_agc/whiteworld/WhiteworldApplication.java
@@ -0,0 +1,13 @@
+package com.blanca_agc.whiteworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blanca_agc/whiteworld/WhiteworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blanca_agc.whiteworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
